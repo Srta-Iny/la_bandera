0cc38dfde0df45faa07667c1d73946fea61303ae
Srta-Iny <iny.mercado@gmail.com>
2016-11-24 19:58:59 -0400

    comentario

--- prueba.js
+++ prueba.js
@@ -63,3 +63,21 @@ function detectBrowser() {
     mapdiv.style.height = '800px';
   }
 }
+function callback(response, status) {
+  if (status == google.maps.DistanceMatrixStatus.OK) {
+    var origins = response.originAddresses;
+    var destinations = response.destinationAddresses;
+
+    for (var i = 0; i < origins.length; i++) {
+      var results = response.rows[i].elements;
+      for (var j = 0; j < results.length; j++) {
+        var element = results[j];
+        var distance = element.distance.text;
+        var duration = element.duration.text;
+        var from = origins[i];
+        var to = destinations[j];
+      }
+    }
+  }
+}
+  
